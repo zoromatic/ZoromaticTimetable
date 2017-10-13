4f16952c7a41f68dfddb3323370f252e5a6086d5
zoromatic <zmatic@carnegietechnologies.com>
2017-10-13 23:24:19 +0200

    Refactoring

--- app/src/main/java/com/zoromatic/timetable/ZoromaticTimetablePreferenceActivity.java
+++ app/src/main/java/com/zoromatic/timetable/ZoromaticTimetablePreferenceActivity.java
@@ -28,23 +28,22 @@ public class ZoromaticTimetablePreferenceActivity extends ThemeAppCompatActivity
         if (existingFragment == null || !existingFragment.getClass().equals(ZoromaticTimetablePreferenceFragment.class)) {
         	prefs = new ZoromaticTimetablePreferenceFragment();
         	String action = getIntent().getAction();
-            
             ActionBar actionBar = getSupportActionBar();
             if (actionBar != null) {
                 actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
                 actionBar.setTitle(R.string.app_prefs);
             }
-            
+
             if (action != null) {
             	Bundle bundle = new Bundle();
                 bundle.putString("category", action);
                 prefs.setArguments(bundle);
-                
+
                 if (actionBar != null) {
     	            if (action.equals(getString(R.string.category_general))) {
     		           actionBar.setTitle(R.string.app_prefs);
     		        } else if (action.equals(getString(R.string.category_theme))) {
-    		        	actionBar.setTitle(R.string.theme_colors);    		        
+    		        	actionBar.setTitle(R.string.theme_colors);
     		        } else {
     		        	actionBar.setTitle(R.string.app_prefs);
     		        }
@@ -55,6 +54,25 @@ public class ZoromaticTimetablePreferenceActivity extends ThemeAppCompatActivity
         	getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, prefs)
                 .commit();
+        } else {
+            String action = getIntent().getAction();
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
+                actionBar.setTitle(R.string.app_prefs);
+            }
+
+            if (action != null) {
+                if (actionBar != null) {
+                    if (action.equals(getString(R.string.category_general))) {
+                        actionBar.setTitle(R.string.app_prefs);
+                    } else if (action.equals(getString(R.string.category_theme))) {
+                        actionBar.setTitle(R.string.theme_colors);
+                    } else {
+                        actionBar.setTitle(R.string.app_prefs);
+                    }
+                }
+            }
         }
     }
 	
